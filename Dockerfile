ede80bd903c4774a6ed8fa022409fcaaa2b50225
Partha Dutta <pdutta@adobe.com>
2018-12-03 17:14:25 -0500

    Convert to Flask app

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
-FROM python:onbuild
+FROM python:alpine3.6
+COPY . /app
+WORKDIR /app
+RUN pip install -r requirements.txt
 ENV PORT 8080
 EXPOSE 8080
 ENTRYPOINT ["python"]
-CMD ["app.py"]
+CMD ["app/app.py"]
---
+++ app/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from flask import Flask
+from flask_restplus import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
